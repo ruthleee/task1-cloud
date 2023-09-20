190fc87a636d131fe8439b4d0b377eea8caf7fc7
Ruth Lee <mona@MacBook-Pro-882.local>
2023-09-20 16:03:47 -0400

    merge

--- resources/students.py
+++ resources/students.py
@@ -8,7 +8,7 @@ class StudentsResource:
     #
 
     students_file = \
-        "/Users/donaldferguson/Dropbox/000-Examples/e6156-microservice-1/resources/old-students.json"
+        "/Users/mona/Desktop/UNI/FALL 2023/CLOUD/task1/resources/old-students.json"
 
     def __init__(self):
         self.students = None
